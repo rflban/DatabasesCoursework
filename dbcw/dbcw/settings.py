6c35069570beb34ec8ca282eae30286512d9b117
rflban <nabiev.alsn@yandex.ru>
2020-09-22 18:51:22 +0300

    change project dbms to postgresql

--- dbcw/dbcw/settings.py
+++ dbcw/dbcw/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dbcw.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portfolios',
+        'USER': 'dbcw_user',
+        'PASSWORD': 'dbcw_password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
